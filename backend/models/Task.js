7beb0471021ef1650165c2af813c93df86da3ab8
mpetkov228 <mpetkov228@gmail.com>
2025-08-02 20:59:28 +0300

    Add userId to task model

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const taskSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     title: { type: String, required: true },
     description: String,
     status: { type: String, enum: ['pending', 'in-progress', 'completed'], default: 'pending' },
