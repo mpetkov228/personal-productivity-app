ae935a575260c54b62a6c1541462a1674cd21684
mpetkov228 <mpetkov228@gmail.com>
2025-08-03 21:01:22 +0300

    Add userId to Habit model

--- backend/models/Habit.js
+++ backend/models/Habit.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const habitSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     name: String,
     frequency: { type: String, enum: ['daily', 'weekly'], default: 'daily' },
     streak: { type: Number, default: 0 },
