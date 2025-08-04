bb898a523b5382d2f2f0a2afacc2b8189f63a446
mpetkov228 <mpetkov228@gmail.com>
2025-08-04 19:54:47 +0300

    Add put and delete to habit router

--- backend/routes/habitRoutes.js
+++ backend/routes/habitRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
 import auth from '../middleware/authMiddleware.js';
-import { createHabit, getHabits } from '../controllers/habitController.js';
+import { createHabit, deleteHabit, getHabits, updateHabit } from '../controllers/habitController.js';
 
 const router = express.Router();
 
 router.use(auth);
 router.get('/', getHabits);
 router.post('/', createHabit);
+router.put('/:id', updateHabit);
+router.delete('/:id', deleteHabit);
 
 export default router;
