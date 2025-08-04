7a40bb8765ebec0c9283962370555241b0b45abf
mpetkov228 <mpetkov228@gmail.com>
2025-08-04 21:05:24 +0300

    Add default value to lastCheckIn

--- backend/models/Habit.js
+++ backend/models/Habit.js
@@ -5,7 +5,7 @@ const habitSchema = new mongoose.Schema({
     name: String,
     frequency: { type: String, enum: ['daily', 'weekly'], default: 'daily' },
     streak: { type: Number, default: 0 },
-    lastCheckIn: Date
+    lastCheckIn: { type: Date, default: new Date() }
 });
 
 export default mongoose.model('Habit', habitSchema);
