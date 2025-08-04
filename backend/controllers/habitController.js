adcadab07c7782222c151b03f8f02ed770f0b475
mpetkov228 <mpetkov228@gmail.com>
2025-08-04 19:31:44 +0300

    Add update and deletion to habit resource

--- backend/controllers/habitController.js
+++ backend/controllers/habitController.js
@@ -13,3 +13,29 @@ export const createHabit = async (req, res) => {
         res.status(400).json({ error: err.message });
     }
 };
+
+export const updateHabit = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const updated = await Habit.findOneAndUpdate(
+            { _id: id }, 
+            req.body,
+            { new: true }
+        );
+        res.json(updated);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+export const deleteHabit = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        await Habit.findOneAndDelete({ _id: id, userId: req.userId });
+        res.status(200).json({ message: 'Habit deleted' });
+    } catch (err) {
+        res.status(400).json({ error: 'Deletion failed' });
+    }
+};
