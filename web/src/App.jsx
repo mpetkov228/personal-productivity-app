d36d72b040718be57a66297dc63641c00d8d86f8
mpetkov228 <mpetkov228@gmail.com>
2025-08-06 15:00:19 +0300

    Configure tailwindcss and start work on Login and Register pages

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,24 +1,17 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
