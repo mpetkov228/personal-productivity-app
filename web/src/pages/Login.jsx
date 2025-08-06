4fee418b08584d7f0e39397e8493a4d987d344da
mpetkov228 <mpetkov228@gmail.com>
2025-08-06 18:54:58 +0300

    Redirect to dashboard when logged in

--- web/src/pages/Login.jsx
+++ web/src/pages/Login.jsx
@@ -1,5 +1,51 @@
+import { useState } from 'react';
+import { useNavigate, Navigate } from 'react-router-dom';
+import axios from 'axios';
+
 export default function Login() {
-    return (
-        <div>Login Page</div>
-    );
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post('http://localhost:8000/api/auth/login', {
+        email,
+        password
+      });
+      localStorage.setItem('token', res.data.token);
+      navigate('/dashboard');
+    } catch (err) {
+      alert('Login failed!');
+    }
+  };
+
+  if (localStorage.getItem('token')) {
+    return <Navigate replace to="/dashboard" />
+  }
+
+  return (
+    <div className="flex h-screen w-screen justify-center items-center bg-gray-100">
+      <form onSubmit={handleLogin} className="bg-white p-6 rounded shadow-md w-80">
+        <h2 className="text-xl font-bold mb-4">Welcome back</h2>
+        <input
+          type="email"
+          placeholder="Email"
+          className="mb-2 w-full p-2 border rounded"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <input 
+          type="password" 
+          placeholder="Password" 
+          className="mb-2 w-full p-2 border rounded"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button type="submit" className="bg-blue-500 text-white p-2 rounded w-full cursor-pointer">Log in</button>
+      </form>
+    </div>
+  );
 }
--- web/src/pages/Register.jsx
+++ web/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 export default function Register() {
-    return (
-        <div>Register Page</div>
-    );
+  return (
+    <div>Register Page</div>
+  );
 }
